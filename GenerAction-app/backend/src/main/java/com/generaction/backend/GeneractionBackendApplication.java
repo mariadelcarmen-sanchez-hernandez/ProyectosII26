c4c75a040a1003f7cb29a72c6f03c53c13ba8d68
María del Carmen Sánchez Hernández <9306744@alumnos.ufv.es>
2026-01-28 19:04:04 +0100

    commit primer paso de la application GenerAction-backend

---
+++ GenerAction-app/backend/src/main/java/com/generaction/backend/GeneractionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.generaction.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneractionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneractionBackendApplication.class, args);
+	}
+
+}
